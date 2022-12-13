f9d42f200579a23448851742724181aa6e9a4d81
Michael Chisolm <aj2vm@yahoo.com>
2022-12-13 14:04:51 -0500

    Add Analyzer, initially just sets the esp app desc datatype

---
+++ src/main/java/esp32_loader/datatype/EspAppDesc.java
@@ -0,0 +1,64 @@
+package esp32_loader.datatype;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import ghidra.app.util.bin.BinaryReader;
+import ghidra.app.util.bin.StructConverter;
+import ghidra.app.util.bin.format.MemoryLoadable;
+import ghidra.app.util.bin.format.Writeable;
+import ghidra.program.model.data.ArrayDataType;
+import ghidra.program.model.data.ByteDataType;
+import ghidra.program.model.data.CategoryPath;
+import ghidra.program.model.data.CharDataType;
+import ghidra.program.model.data.DWordDataType;
+import ghidra.program.model.data.DataType;
+import ghidra.program.model.data.StructureDataType;
+import ghidra.util.DataConverter;
+import ghidra.util.exception.DuplicateNameException;
+
+public class EspAppDesc implements StructConverter, Comparable<EspAppDesc>, Writeable, MemoryLoadable {
+	long magic_word;
+	long secure_version;
+	int[] reserv1;
+	char[] version;
+	char[] project_name;
+	char[] time;
+	char[] date;
+	char[] idf_ver;
+	byte[] app_elf_sha256;
+	long[] reserv2;
+	
+	public EspAppDesc(BinaryReader reader) throws IOException {
+		magic_word = reader.readNextUnsignedInt();
+		secure_version = reader.readNextUnsignedInt();
+	}
+
+	@Override
+	public void write(RandomAccessFile raf, DataConverter dc) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int compareTo(EspAppDesc o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DataType toDataType() throws DuplicateNameException, IOException {
+		StructureDataType struct = new StructureDataType(new CategoryPath("/ESP32"), "esp_app_desc_t", 0);
+		struct.add(DWordDataType.dataType, "magic_word", null);
+		struct.add(DWordDataType.dataType, "secure_version", null);
+		struct.add(new ArrayDataType(DWordDataType.dataType, 2, 0), "reserv1", null);
+		struct.add(new ArrayDataType(CharDataType.dataType, 32, 0), "version", null);
+		struct.add(new ArrayDataType(CharDataType.dataType, 32, 0), "project_name", null);
+		struct.add(new ArrayDataType(CharDataType.dataType, 16, 0), "time", null);
+		struct.add(new ArrayDataType(CharDataType.dataType, 16, 0), "date", null);
+		struct.add(new ArrayDataType(CharDataType.dataType, 32, 0), "idf_ver", null);
+		struct.add(new ArrayDataType(ByteDataType.dataType, 32, 0), "app_elf_sha256", null);
+		struct.add(new ArrayDataType(DWordDataType.dataType, 20, 0), "reserv2", null);
+		return struct;
+	}
+}
