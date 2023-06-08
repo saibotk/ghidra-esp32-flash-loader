b9e25b407388b771df1ec3f91bfa3010b025f955
Austin Conn <austinc3030@gmail.com>
2023-06-08 14:34:02 -0400

    Pull in special mike's fork

--- src/main/java/esp32_loader/flash/ESP32AppSegment.java
+++ src/main/java/esp32_loader/flash/ESP32AppSegment.java
@@ -6,167 +6,93 @@ import ghidra.app.util.bin.BinaryReader;
 
 public class ESP32AppSegment {
 
+	public enum SegmentType {
+		DRAM0, DRAM1, IF_TXT, IRAM0, IRAM1, DF_ROA, F_DATA, IRAM, IROM0, DROM0
+	}
+
 	public int PhysicalOffset;
-    public int LoadAddress;
-    public int Length;
-    public byte[] Data;
-    
-    public boolean IsRead = false;
-    public boolean IsWrite = false;
-    public boolean IsExecute = false;
-    public String SegmentName;
-    public boolean IsEsp32 = false;
-    
+	public int LoadAddress;
+	public int Length;
+	public byte[] Data;
+
+	public SegmentType type;
+	public boolean IsEsp32 = false;
+
 	public ESP32AppSegment(ESP32AppImage app, BinaryReader reader, boolean isEsp32S2) throws IOException {
 		// TODO Auto-generated constructor stub
-		IsEsp32=isEsp32S2;
+		IsEsp32 = isEsp32S2;
 		LoadAddress = reader.readNextInt();
-        Length = reader.readNextInt();
-        Data = reader.readNextByteArray(Length);
-        /* fully consume the segment */
-        if (isEsp32S2) {
-			
+		Length = reader.readNextInt();
+		Data = reader.readNextByteArray(Length);
+		/* fully consume the segment */
+		if (isEsp32S2) {
 			/* determine access type via memory map */
 			/*
-				Loading section .flash.rodata, size 0x576c lma 0x3f000020   DROM0
-				Loading section .dram0.data, size 0x1e74 lma 0x3ffbe150
-				Loading section .iram0.vectors, size 0x403 lma 0x40024000
-				Loading section .iram0.text, size 0x9d40 lma 0x40024404
-				Loading section .flash.text, size 0x147f7 lma 0x40080020
-			*/
-	                // OK
-			if( LoadAddress >= 0x3FFB0000 && LoadAddress <= 0x3FFB7FFF  ) {
-				IsExecute = true;
-				IsRead = true;
-				IsWrite = true;
-				SegmentName = "DRAM0";
-				return;
+			 * Loading section .flash.rodata, size 0x576c lma 0x3f000020 DROM0 Loading
+			 * section .dram0.data, size 0x1e74 lma 0x3ffbe150 Loading section
+			 * .iram0.vectors, size 0x403 lma 0x40024000 Loading section .iram0.text, size
+			 * 0x9d40 lma 0x40024404 Loading section .flash.text, size 0x147f7 lma
+			 * 0x40080020
+			 */
+			if (LoadAddress >= 0x3FFB0000 && LoadAddress <= 0x3FFB7FFF) {
+				type = SegmentType.DRAM0;
+			} else if (LoadAddress >= 0x3FFB8000 && LoadAddress <= 0x3FFFFFFF) {
+				type = SegmentType.DRAM1;
+			} else if (LoadAddress >= 0x40080000 && LoadAddress <= 0x40080000 + 4194304) {
+				type = SegmentType.IF_TXT;
+			} else if (LoadAddress >= 0x40020000 && LoadAddress <= 0x40027FFF) {
+				type = SegmentType.IRAM0;
+			} else if (LoadAddress >= 0x40028000 && LoadAddress <= 0x4006FFFF) {
+				type = SegmentType.IRAM1;
+			} else if (LoadAddress >= 0x3F000000 && LoadAddress <= 0x3F3F0000) {
+				type = SegmentType.DF_ROA;
+			} else if (LoadAddress >= 0x3F800000 && LoadAddress <= 0x3F800000 + 4194304) {
+				type = SegmentType.F_DATA;
+			} else {
+				type = SegmentType.IRAM;
 			}
-
-			//OK
-			if( LoadAddress >= 0x3FFB8000 && LoadAddress <= 0x3FFFFFFF  ) {
-				IsExecute = true;
-				IsRead = true;
-				IsWrite = true;
-				SegmentName = "DRAM1";
-				return;
-			}
-			
-			// OK
-			if( LoadAddress >= 0x40080000 && LoadAddress <= 0x40080000 + 4194304) {
-				IsExecute = true;
-				IsRead = true;
-				SegmentName = "IF_TXT";
-				return;
-			}
-			// OK
-			if( LoadAddress >= 0x40020000 && LoadAddress <= 0x40027FFF  ) {
-				IsExecute = true;
-				IsRead = true;
-				IsWrite = true;
-				SegmentName = "IRAM0";
-				return;
-			}
-			// OK
-			if( LoadAddress >= 0x40028000 && LoadAddress <= 0x4006FFFF) {
-				IsExecute = true;
-				IsRead = true;
-				IsWrite = true;
-				SegmentName = "IRAM1";
-				return;
-			}
-			
-			// OK
-			if( LoadAddress >= 0x3F000000 && LoadAddress <= 0x3F3F0000) {
-				IsExecute = false;
-				IsRead = true;
-				IsWrite = false;
-				SegmentName = "DF_ROA";
-				return;
-			}
-			
-			// ???
-			/*
-			if( LoadAddress >= 0x3F080000 && LoadAddress <= 0x3F080000 + 524288) {
-				IsExecute = false;
-				IsRead = true;
-				IsWrite = true;
-				SegmentName = "DRAM0";
-				return;
-			}
-			*/
-			if( LoadAddress >= 0x3F800000 && LoadAddress <= 0x3F800000 + 4194304) {
-				IsExecute = false;
-				IsRead = false;
-				IsWrite = true;
-				SegmentName = "F_DATA";
-				return;
-			}        
-
-			IsExecute = true;
-			IsRead = true;
-			IsWrite = true;
-			SegmentName = "IRAM";
-			return;
-
 
 		} else {
-			
 			/* determine access type via memory map */
-			if( LoadAddress >= 0x40800000 && LoadAddress <= 0x40800000 + 4194304) {
-				IsExecute = true;
-				IsRead = true;
-				SegmentName = "IROM0";
-				return;
+			if (LoadAddress >= 0x40800000 && LoadAddress <= 0x40800000 + 4194304) {
+				type = SegmentType.IROM0;
+			} else if (LoadAddress >= 0x40000000 && LoadAddress <= 0x40000000 + 4194304) {
+				type = SegmentType.IRAM0;
+			} else if (LoadAddress >= 0x40400000 && LoadAddress <= 0x40400000 + 4194304) {
+				type = SegmentType.IRAM1;
+			} else if (LoadAddress >= 0x3F400000 && LoadAddress <= 0x3F400000 + 4194304) {
+				type = SegmentType.DROM0;
+			} else if (LoadAddress >= 0x3FF80000 && LoadAddress <= 0x3FF80000 + 524288) {
+				type = SegmentType.DRAM0;
+			} else if (LoadAddress >= 0x3F800000 && LoadAddress <= 0x3F800000 + 4194304) {
+				type = SegmentType.DRAM1;
+			} else {
+				type = SegmentType.IRAM;
 			}
-			
-			if( LoadAddress >= 0x40000000 && LoadAddress <= 0x40000000 + 4194304) {
-				IsExecute = true;
-				IsRead = true;
-				IsWrite = true;
-				SegmentName = "IRAM0";
-				return;
-			}
-			
-			if( LoadAddress >= 0x40400000 && LoadAddress <= 0x40400000 + 4194304) {
-				IsExecute = true;
-				IsRead = true;
-				IsWrite = true;
-				SegmentName = "IRAM1";
-				return;
-			}
-			
-			if( LoadAddress >= 0x3F400000 && LoadAddress <= 0x3F400000 + 4194304) {
-				IsExecute = false;
-				IsRead = true;
-				IsWrite = false;
-				SegmentName = "DROM0";
-				return;
-			}
-			
-			if( LoadAddress >= 0x3FF80000 && LoadAddress <= 0x3FF80000 + 524288) {
-				IsExecute = false;
-				IsRead = true;
-				IsWrite = true;
-				SegmentName = "DRAM0";
-				return;
-			}
-			
-			if( LoadAddress >= 0x3F800000 && LoadAddress <= 0x3F800000 + 4194304) {
-				IsExecute = false;
-				IsRead = true;
-				IsWrite = true;
-				SegmentName = "DRAM1";
-				return;
-			}        
-
-			IsExecute = true;
-			IsRead = true;
-			IsWrite = true;
-			SegmentName = "IRAM";
-			return;
-
 		}
 	}
 
+	public boolean isWrite() {
+		// There are definitely code segments that are writable, but it seems like it is
+		// common for the compiler to stick function pointers in the code
+		// segments. This means the decompiler will show the pointer being casted to
+		// `code *` and called, making it more difficult to read. If the
+		// pointers are not writable, then the decompiler will helpfully show a function
+		// pointer call as a normal function call.
+		return this.type != null && this.type != SegmentType.DROM0 && this.type != SegmentType.IROM0
+				&& !this.isCodeSegment();
+	}
+
+	public boolean isRead() {
+		return this.type != null && this.type != SegmentType.F_DATA;
+	}
+
+	public boolean isExecute() {
+		return this.isCodeSegment();
+	}
+
+	public boolean isCodeSegment() {
+		return this.type != null && (this.type == SegmentType.IF_TXT || this.type == SegmentType.IRAM
+				|| this.type == SegmentType.IRAM0 || this.type == SegmentType.IRAM1 || this.type == SegmentType.IROM0);
+	}
 }
--- src/main/java/esp32_loader/flash/ESP32Flash.java
+++ src/main/java/esp32_loader/flash/ESP32Flash.java
@@ -8,24 +8,24 @@ import ghidra.app.util.bin.BinaryReader;
 public class ESP32Flash {
 	public ESP32AppImage SecondaryBootloader;
 	public ArrayList<ESP32Partition> Partitions = new ArrayList<ESP32Partition>();
-	
+
 	public ESP32Flash(BinaryReader reader) throws IOException {
-		
+
 		/* first 0x1000 bytes are empty */
 		byte[] skipped = reader.readNextByteArray(0x1000);
 		var idx1 = reader.getPointerIndex();
 		byte[] bootLoader = reader.readNextByteArray(0x7000);
-		
+
 		var idx2 = reader.getPointerIndex();
 		/* should be at the partition table now */
-		while (reader.peekNextShort() == 0x50AA){
+		while (reader.peekNextShort() == 0x50AA) {
 			var part = new ESP32Partition(reader);
 			Partitions.add(part);
 		}
 	}
-	
+
 	public ESP32Partition GetPartitionByName(String name) {
-		for(var x =0; x < Partitions.size(); x++) {
+		for (var x = 0; x < Partitions.size(); x++) {
 			if (Partitions.get(x).Name.equals(name)) {
 				return Partitions.get(x);
 			}
